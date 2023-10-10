87a12144c0639f156d5e389ba39cb843a8c5d34b
Jose Perez <jose@scholarfundwa.org>
2023-10-10 11:44:38 -0700

    tracks

--- main.py
+++ main.py
@@ -3,5 +3,11 @@ Prints the number of pages crawled. (Does not have any crawling code,
 so the number is always 0.)
 """
 
+import requests
+
+
+
+print ("Using requests version:", requests._version_)
+
 num_pages = 0
 print("Found", num_pages, "pages.")
