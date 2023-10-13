681439e49353c25d722012b48d0a41f0259d09df
Jose Perez <jose@scholarfundwa.org>
2023-10-13 12:17:46 -0700

    tracks

--- main.py
+++ main.py
@@ -7,7 +7,10 @@ import requests
 
 
 
-print ("Using requests version:", requests._version_)
+print ("Using requests version:", requests.__version__)
 
 num_pages = 0
 print("Found", num_pages, "pages.")
+
+response =  requests.get("https://www.scholarfundwa.org/")
+print(response.content)
