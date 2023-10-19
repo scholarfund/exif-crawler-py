02aa34734631b04dc49195babcc135449755868e
Jose Perez <jose@scholarfundwa.org>
2023-10-19 10:19:12 -0700

    quit

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 """
-Here is a simple web browser that is looping constantly, if the URL doesn't have "https://" it will add it for the browser to work.
+Here is a simple web browser that is looping constantly unless you ask it to "quit", if the URL doesn't have "https://" it will add it for the browser to work.
 """
 
 
 
 while True: # The "True" tells Python to always try to loop.
-    url = input("Enter a URL: ")
+    url = input("Enter a URL (or 'quit' to exit): ")
+    if url.lower() == "quit":
+        break
     if "https://" not in url: 
         url = "https://" + url 
     import requests
