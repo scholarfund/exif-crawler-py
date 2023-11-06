aedf0eb91a3abb569d01290e0a62575ad508067f
Jose Perez <jose@scholarfundwa.org>
2023-11-06 12:25:53 -0800

    BeautifulSoup

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 Here is a simple web browser that is looping constantly unless you ask it to "quit", if the URL doesn't have "https://" it will add it for the browser to work.
 """
 import requests
+import bs4
 
 
 while True: # The "True" tells Python to always try to loop.
@@ -11,4 +12,16 @@ while True: # The "True" tells Python to always try to loop.
     if "https://" not in url: 
         url = "https://" + url 
     response =  requests.get(url)
-    print(response.content)
+    #print(response.content)
+    html_doc = response.content
+    soup = bs4.BeautifulSoup(html_doc, 'html.parser')
+    print(soup.prettify())
+
+# Output:
+
+# <!DOCTYPE html>
+# <html>
+#   <body>
+#     Hello, World!
+#   </body>
+# </html>
